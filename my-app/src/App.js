081993f8e37ebe1b5062acb1b1490fff18b14721
Karanjot Singh <113842752+Karanxidhu@users.noreply.github.com>
2023-09-03 23:39:49 +0530

    initial design

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css"
+import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Routes>
+      <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Home.js
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='min-h-[100vh] bg-[#181818] w-full'>
+            <div className='min-w-fit md:min-w-min w-1/2 mx-auto'>
+                <div className='w-[95%] mx-auto py-2'>
+                    <h1 className='text-base text-[#4C8D4C] font-extrabold'>KaranXidhu<span className='text-slate-300 text-sm'>:<span className='text-[#0096FF]'>~</span>$ cd ./<a href="/" className='font-thin text-[#7cafc2] underline p-1'>projects</a></span></h1>
+                </div>
+                <div className='w-full bg-[#282828] p-3 pt-7 leading-4'>
+                    <span className='text-sm text-[#4C8D4C] font-bold'>KaranXidhu</span><span className='text-slate-300 text-sm font-bold'>:</span><span className='text-[#0096FF]'>~</span><span className='text-sm font-bold text-slate-300'>$ neofetch</span>
+                    <p className='leading-3 pt-1 text-[#EE4B2B] font-bold text-sm'>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.-/+oossssoo+/-.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;karanxidhu<span className='text-slate-400'>@</span>karanxidhu<br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`:+ssssssssssssssssss+:`&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span className='text-slate-300'>---------------------</span><br />
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-+ssssssssssssssssssyyssss+-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;OS:<span className='text-slate-300'>&nbsp;Ubuntu&nbsp;22.10&nbsp;x86_64</span><br />
+
+                        &nbsp;&nbsp;&nbsp;&nbsp;.ossssssssssssssssss<span className='text-slate-300'>dMMMNy</span>sssso.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Host:<span className='text-slate-300'>&nbsp;GF65&nbsp;Thin&nbsp;9SD&nbsp;REV:1.0</span><br />
+
+                        &nbsp;&nbsp;&nbsp;/sssssssssss<span className='text-slate-300'>hdmmNNmmyNMMMMh</span>ssssss/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Kernel:<span className='text-slate-300'>&nbsp;5.19.0-46-generic</span><br />
+
+                        &nbsp;&nbsp;+sssssssss<span className='text-slate-300'>hm</span>yd<span className='text-slate-300'>MMMMMMMNddddy</span>ssssssss+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Uptime:<span className='text-slate-300'>&nbsp;3&nbsp;hours,&nbsp;17&nbsp;mins</span><br />
+
+                        &nbsp;/ssssssss<span className='text-slate-300'>hNMMM</span>yh<span className='text-slate-300'>hyyyyhmNMMMNh</span>ssssssss/&nbsp;&nbsp;&nbsp;&nbsp;Packages:<span className='text-slate-300'>&nbsp;2771&nbsp;(dpkg),&nbsp;20&nbsp;(snap)</span><br />
+
+                        .ssssssss<span className='text-slate-300'>dMMMNh</span>ssssssssss<span className='text-slate-300'>hNMMMd</span>ssssssss.&nbsp;&nbsp;&nbsp;Shell:<span className='text-slate-300'>&nbsp;bash&nbsp;5.2.2</span><br />
+
+                        +ssss<span className='text-slate-300'>hhhyNMMNy</span>ssssssssssss<span className='text-slate-300'>yNMMMy</span>sssssss+&nbsp;&nbsp;&nbsp;Resolution:<span className='text-slate-300'>&nbsp;1920x1080</span><br />
+
+                        oss<span className='text-slate-300'>yNMMMNyMMh</span>ssssssssssssss<span className='text-slate-300'>hmmmh</span>ssssssso&nbsp;&nbsp;&nbsp;DE:<span className='text-slate-300'>&nbsp;GNOME&nbsp;43.1</span><br />
+
+                        oss<span className='text-slate-300'>yNMMMNyMMh</span>sssssssssssssshmmmhssssssso&nbsp;&nbsp;&nbsp;WM:<span className='text-slate-300'>&nbsp;Mutter</span><br />
+
+                        +ssss<span className='text-slate-300'>hhhyNMMNy</span>ssssssssssss<span className='text-slate-300'>yNMMMy</span>sssssss+&nbsp;&nbsp;&nbsp;WM&nbsp;Theme:<span className='text-slate-300'>&nbsp;Adwaita</span><br />
+
+                        .ssssssss<span className='text-slate-300'>dMMMNh</span>ssssssssss<span className='text-slate-300'>hNMMMd</span>ssssssss.&nbsp;&nbsp;&nbsp;Theme:<span className='text-slate-300'>&nbsp;WhiteSur-Dark&nbsp;[GTK2/3]</span><br />
+
+                        &nbsp;/ssssssss<span className='text-slate-300'>hNMMM</span>yh<span className='text-slate-300'>hyyyyhdNMMMNh</span>ssssssss/&nbsp;&nbsp;&nbsp;&nbsp;Icons:<span className='text-slate-300'>&nbsp;Cupertino-Ventura&nbsp;[GTK2/3]</span><br />
+
+                        &nbsp;&nbsp;+sssssssss<span className='text-slate-300'>dm</span>yd<span className='text-slate-300'>MMMMMMMMddddy</span>ssssssss+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Terminal:<span className='text-slate-300'>&nbsp;gnome-terminal</span><br />
+
+                        &nbsp;&nbsp;&nbsp;/sssssssssss<span className='text-slate-300'>hdmNNNNmyNMMMMh</span>ssssss/&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CPU:<span className='text-slate-300'>&nbsp;Intel&nbsp;i5-9300H&nbsp;(8)&nbsp;@&nbsp;4.100GHz</span><br />
+
+                        &nbsp;&nbsp;&nbsp;&nbsp;.ossssssssssssssssss<span className='text-slate-300'>dMMMNy</span>sssso.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GPU:<span className='text-slate-300'>&nbsp;NVIDIA&nbsp;GeForce&nbsp;GTX&nbsp;1660&nbsp;Ti&nbsp;Mobi</span><br />
+
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;-+sssssssssssssssss<span className='text-slate-300'>yyy</span>ssss+-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GPU:<span className='text-slate-300'>&nbsp;Intel&nbsp;CoffeeLake-H&nbsp;GT2&nbsp;[UHD&nbsp;Gra</span><br />
+
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;`:+ssssssssssssssssss+:`&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Memory:<span className='text-slate-300'>&nbsp;4945MiB&nbsp;/&nbsp;15772MiB</span><br />
+                    </p>
+                    <div className="pt-7">
+                        <span className='text-sm text-[#4C8D4C] font-bold'>KaranXidhu</span><span className='text-slate-300 text-sm font-bold'>:</span><span className='text-[#0096FF]'>~</span><span className='text-sm font-bold text-slate-300'>$ cd ./<span className='text-xl'>📜 Behind the Pixels 📜</span></span>
+                        <p className='w-[100%] p-2 text-sm font-bold text-slate-400'>
+                            👋 Hey there! I'm Karan, a student at TIET, where I'm pursuing my passion for Computer Science and Engineering. As a full-stack developer, I enjoy creating user-friendly solutions from start to finish. Lately, I've been diving into the world of machine learning, always eager to learn something new. What truly drives me is the joy of crafting innovative software and solutions. I believe in the power of simplicity and user-centered design. So, let's explore the digital realm together, one step at a time. Join me on this journey, and let's create meaningful tech experiences. 🌟</p>
+
+                    </div>
+                    <div className="pt-7">
+                        <span className='text-sm text-[#4C8D4C] font-bold'>KaranXidhu</span><span className='text-slate-300 text-sm font-bold'>:</span><span className='text-[#0096FF]'>~</span><span className='text-sm font-bold text-slate-300'>$ cd ./<span className='text-xl'>🌇 Unscripted Adventures 🌇</span></span>
+                        <p className='p-2 text-sm font-bold text-slate-400'>
+                            <span className="text-slate-300">Currently</span>, I proudly serve as the <span className="text-slate-300">tech head</span> at <a href="/" className='font-thin text-[#7cafc2] underline'> CONCEPTOU</a>, where I've taken on the responsibility of leading a dynamic team. This role has been a remarkable journey of growth, where I've not only honed my technical skills but also cultivated <span className="text-slate-300">leadership and teamwork</span>. Guiding and collaborating with my team members as we navigate the ever-evolving tech landscape has been an enriching experience, and I'm excited to continue charting our course to success.🚀 
+                        </p>
+                        <p className='p-2 text-sm font-bold text-slate-400'>
+                            As a <span className="text-slate-300">web developer</span> intern at <span className="text-slate-300"><a href="/" className='font-thin text-[#7cafc2] underline'> CONCEPTOU</a></span>, I had the opportunity to roll up my sleeves and bring digital concepts to life. I contributed to the development of engaging <span className="text-slate-300">websites</span> while simultaneously dipping my toes into the fascinating world of <span className="text-slate-300">machine learning</span>. This experience allowed me to bridge the gap between creative design and technical innovation, sparking my passion for both.🌐 </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
